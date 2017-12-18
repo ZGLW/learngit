03d8c149d0598321f3d3b4bad389f6abaaf6bd5b
Leo <514344846@qq.com>
2017-12-18 21:21:17 +0800

    create a git project

---
+++ sample.cpp
@@ -0,0 +1,14 @@
+/*************************************************************************
+	> File Name: sample.cpp
+	> Author:liwei 
+	> Mail: 
+	> Created Time: 2017年12月18日 星期一 21时20分01秒
+ ************************************************************************/
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout<<"Hello Git!"<<endl;
+    return 0;
+}
